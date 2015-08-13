8d8346d612e401d7b44a04a121881f66e5a43cf6
Jordan Johnson <jmj@fellowhuman.com>
2015-08-13 08:28:26 -0700

    Fix build deps.

--- info.rkt
+++ info.rkt
@@ -1,8 +1,9 @@
 #lang info
 (define collection "option-bind")
 (define deps '("base" "typed-racket-lib"))
-(define build-deps '("scribble-lib" "racket-doc" "typed-racket-lib"))
+(define build-deps '("scribble-lib" "racket-doc" "typed-racket-lib"
+                     "typed-racket-more"))
 (define scribblings '(("scribblings/option-bind.scrbl" ())))
 (define pkg-desc "Shortcuts for applying TR functions to Option values.")
-(define version "1.0")
+(define version "1.0.1")
 (define pkg-authors '(jteach))
